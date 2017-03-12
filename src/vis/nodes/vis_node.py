cb92fe27fe8b7c31d69189f2fb809a21a81d1985
Vijay <vijay.somers@gmail.com>
2017-03-12 19:17:21 -0400

    Still trying to get it to read an image

--- src/vis/nodes/vis_node.py
+++ src/vis/nodes/vis_node.py
@@ -1,20 +1,32 @@
 #!/usr/bin/env python
 import rospy
 import cv2
+import numpy as np
 
 
+def shutdownActions():
+    cv2.destroyAllWindows()
 
 def main():
     #pub = rospy.Publisher('chatter', String, queue_size=10)
     rospy.init_node('vis', anonymous=True)
     rate = rospy.Rate(10) # 10hz
-    m = cv2.cvtColor((200,200))
+    #rospy.on_shutdown(shutdownActions)
+    image_path = "~/hovercat.jpg"
+    image = cv2.imread(image_path, cv2.IMREAD_COLOR)
+    print image.size
+
+    #size = 200, 200
+    #m = np.zeros(size, dtype=np.uint8)
+    cv2.namedWindow("draw", cv2.CV_WINDOW_AUTOSIZE)
 
     while not rospy.is_shutdown():
-        cv2.namedWindow("stuff")
+
         hello_str = "hello wod %s" % rospy.get_time()
         rospy.logerr(hello_str)
         #pub.publish(hello_str)
+        cv2.imshow("draw", image)
+        cv2.waitKey(1)
         rate.sleep()
 
 if __name__ == '__main__':
