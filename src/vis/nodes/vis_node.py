d136b3a7b7858c86a674cd95edb990871199dd21
Vijay <vijay.somers@gmail.com>
2017-03-12 01:33:06 -0500

    stuff

--- src/vis/nodes/vis_node.py
+++ src/vis/nodes/vis_node.py
@@ -1,11 +1,24 @@
+#!/usr/bin/env python
 import rospy
+import cv2
+
+
 
 def main():
-    pass
-    print "XXXXXXXXXXXXXXXX"
-    #rospy.init_node('vis_node', anonymous=False)
+    #pub = rospy.Publisher('chatter', String, queue_size=10)
+    rospy.init_node('vis', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    m = cv2.cvtColor((200,200))
 
-    # Rate in Hz
-    rate = rospy.Rate(20)
-    print "hello wod"
-    rate.sleep()
+    while not rospy.is_shutdown():
+        cv2.namedWindow("stuff")
+        hello_str = "hello wod %s" % rospy.get_time()
+        rospy.logerr(hello_str)
+        #pub.publish(hello_str)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
